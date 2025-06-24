6bf634c51da5ca12cbda8719697491d89bf9b642
Eliyaan <nopana1007@gmail.com>
2025-06-24 17:25:31 +0200

    5.2

--- rtc.glsl
+++ rtc.glsl
@@ -31,7 +31,19 @@ vec3 at(in Ray r, in float t) {
 	return r.orig + t * r.dir;
 }
 
+bool hit_sphere(in vec3 center, in float radius, in Ray r) {
+    vec3 oc = center - r.orig;
+    float a = dot(r.dir, r.dir);
+    float b = -2.0 * dot(r.dir, oc);
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 vec3 ray_color(in Ray r) {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+		return vec3(1.0, 0.0, 0.0);
+	}
 	vec3 unit_direction = normalize(r.dir);
 	float a = 0.5*(unit_direction.y + 1.0);
 	return (1.0-a)*vec3(1.0, 1.0, 1.0) + a*vec3(0.5, 0.7, 1.0);
@@ -42,16 +54,17 @@ void main() {
 
 	// for all the pixels:
 	float focal_length = 1.0;
-	vec3 cam_center = vec3(0, 0, 0);
+	vec3 cam_center = vec3(0.0, 0.0, 0.0);
 
-	vec3 viewport = vec3(2.0, 2.0, 0);
+	float aspect_ratio = res.x/res.y;
+	vec3 viewport = vec3(2.0*aspect_ratio, 2.0, 0);
 
-	vec3 pixel_delta = vec3(2.0/res.x, 2.0/res.y, 0.0); // from -1 to 1
-	vec3 viewport_lower_left = cam_center - vec3(0.0, 0.0, focal_length) - viewport.x / 2.0 - viewport.y / 2.0;
+	vec3 pixel_delta = vec3(viewport.x/res.x, viewport.y/res.y, 0.0); // the viewport is from -1 to 1
+	vec3 viewport_lower_left = cam_center - vec3(0.0, 0.0, focal_length) - vec3(viewport.x / 2.0, viewport.y / 2.0, 0.0);
 	vec3 pixel00_loc = viewport_lower_left + 0.5 * pixel_delta;
 
 	// for this pixel:
-	vec3 pixel_center = pixel00_loc + (gl_FragCoord.x * pixel_delta.x) + (gl_FragCoord.y * pixel_delta.y);
+	vec3 pixel_center = pixel00_loc + vec3(gl_FragCoord.x * pixel_delta.x, gl_FragCoord.y * pixel_delta.y, 0.0);
 	vec3 ray_direction = pixel_center - cam_center;
 	Ray r = Ray(cam_center, ray_direction);
 
