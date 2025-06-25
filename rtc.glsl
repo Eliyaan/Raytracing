a2b5f6a9592fce9b4764d193675e0b62d6bcf9ea
Eliyaan <nopana1007@gmail.com>
2025-06-25 22:18:01 +0200

    6.7

--- rtc.glsl
+++ rtc.glsl
@@ -13,6 +13,14 @@ void main() {
 
 @fs fs
 
+const float pi = 3.14159265;
+const float inf = 1000000000000.0;
+
+
+float deg2rad(float deg) {
+	return deg * pi / 180.0;
+}
+
 uniform fs_params {
 	vec2 res;
 	vec2 mouse;
@@ -35,6 +43,8 @@ struct Ray {
 	vec3 dir;
 };
 
+const Sphere hittables[2] = {Sphere(vec3(0.0, 0.0, -1.0), 0.5), Sphere(vec3(0.0, -100.5, -1.0), 100.0)};
+
 in vec4 pos;
 
 out vec4 frag_color;
@@ -43,14 +53,14 @@ vec3 at(in Ray r, in float t) {
 	return r.orig + t * r.dir;
 }
 
-void set_face_normal(out HitRecord rec, in Ray r, in vec3 outward_normal) {
+void set_face_normal(inout HitRecord rec, in Ray r, in vec3 outward_normal) {
 	// outward_normal is assumed to have unit length
 
 	rec.front_face = dot(r.dir, outward_normal) < 0.0;
 	rec.normal = rec.front_face ? outward_normal : -outward_normal;
 }
 
-bool hit_sphere(in Sphere s, in Ray r, in float ray_tmin, float ray_tmax, out HitRecord rec) {
+bool hit_sphere(in Sphere s, in Ray r, in float ray_tmin, in float ray_tmax, out HitRecord rec) {
 	vec3 oc = s.center - r.orig;
 	float a = dot(r.dir, r.dir);
 	float h = dot(r.dir, oc);
@@ -60,6 +70,7 @@ bool hit_sphere(in Sphere s, in Ray r, in float ray_tmin, float ray_tmax, out Hi
 	if (discriminant < 0) {
 		return false;
 	}
+
 
 	float sqrtd = sqrt(discriminant);
 	// find the nearest root that lies in the acceptable range
@@ -79,17 +90,33 @@ bool hit_sphere(in Sphere s, in Ray r, in float ray_tmin, float ray_tmax, out Hi
 	return true;
 }
 
-vec3 ray_color(in Ray r) {
-	vec3 sphere_loc = vec3(0, 0, -1);
-	float t = hit_sphere(sphere_loc, 0.5, r);
-	if (t >= 0.0) {
-		vec3 n = normalize(at(r, t) - sphere_loc);
-		return (n + 1.0)*0.5;
+bool hit_all(in Ray r, in float ray_tmin, in float ray_tmax, out HitRecord rec) {
+	HitRecord tmp_rec;
+	bool hit_anything;
+	float closest_so_far = ray_tmax;
+
+	for (int i = 0; i < hittables.length(); i++) {
+		if (hit_sphere(hittables[i], r, ray_tmin, closest_so_far, tmp_rec)) {
+			hit_anything = true;
+			closest_so_far = tmp_rec.t;
+			rec = tmp_rec;
+		}
 	}
+
+	return hit_anything;
+}
+
+vec3 ray_color(in Ray r) {
+	HitRecord rec;
+	if (hit_all(r, 0, inf, rec)) {
+		return 0.5 * (rec.normal + 1);
+	}
+
 	vec3 unit_direction = normalize(r.dir);
 	float a = 0.5*(unit_direction.y + 1.0);
 	return (1.0-a)*vec3(1.0, 1.0, 1.0) + a*vec3(mouse, 1.0);
 }
+
 
 void main() {
 	// gl_FragCoord
